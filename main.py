5602952a446e1699645f28658f89b6960cef9f0b
Masoud Shokrnezhad <m.shokrnezhad@gmail.com>
2020-07-10 21:44:27 +0430

    Cleaning Code...

--- main.py
+++ main.py
@@ -11,10 +11,7 @@ stock_list = get_stock_list_from_processed_file(stock_list_file_path)
 price_list = get_price_list_from_processed_file(price_list_file_path, len(stock_list), 68)
 
 # we have 68 columns of data.
-# we decide to use the price of past 10 days to predict the price of day 11,
-# so we have X = [1 P_d_-10 P_d_-9 ... P_d_-1] and y = p_d_0.
-# also we decide to use 40 sets of data to train h function.
-# lets go
+# we decide to use 40 sets of data to train the h function.
 
 number_of_training_sets = 40
 number_of_training_features = 3 # 1 + number of days we want to use their price as the training features
